61c67723d689545038d570c3eda8bff9ada01d2c
Arthur Dolan <arthurdolan@Arthurs-MacBook-Air.local>
2024-04-27 21:15:40 +0100

    takes all the names from file and makes person instances with no duplicate names

--- Analysis.java
+++ Analysis.java
@@ -1,6 +1,11 @@
 import java.io.File; // File class
 import java.io.FileNotFoundException; //Handles errors
 import java.util.Scanner; // Reads text files
+import java.util.List;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+
 
 public class Analysis
 {
@@ -12,15 +17,35 @@ public class Analysis
         }
         else
         {
+
+            List<String> arrayOfNames = new ArrayList<>(); //Stores names into a string 
+            Set<String> hashNames = new HashSet<>(); //Stores each name to ensure not added twice
             String inputFileName = args[0]; // Takes first file from command line
             File inputFile = new File(inputFileName);
             try
             {
-                Scanner readFile = new Scanner(inputFile);
+                Scanner readFile = new Scanner(inputFile); 
                 while(readFile.hasNextLine())
                 {
-                    String data = readFile.nextLine();
-                    System.out.println(data);
+ 
+                    String line = readFile.nextLine();
+                    String names[]= line.trim().split("\\s+");//seperates and stores each name in names[]
+                    for(String name : names) //iterates through each name in names
+                    {
+                        if(hashNames.contains(name))
+                        {
+                            //do nothing 
+                        }
+                        else
+                        {
+
+                            Person person = new Person(name, 5);
+                            arrayOfNames.add(name);
+                            hashNames.add(name);
+                            System.out.println("Added name: " + name);
+                        }
+                    }
+                    
                 }
                 readFile.close();
             }   
@@ -32,7 +57,5 @@ public class Analysis
             
 
         }
-
-
     }
 }
